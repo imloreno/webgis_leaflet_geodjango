5615e747a77fcc027e6279a3c597b62e8b62356e
imloreno <work@imloreno.com>
2024-10-11 07:56:50 -0400

    Adding models for Capitals, States, and Rivers'

--- app/service/models.py
+++ app/service/models.py
@@ -1,3 +1,28 @@
-from django.db import models
+from django.contrib.gis.db import models
 
-# Create your models here.
+class Capital(models.Model):
+    id = models.IntegerField(primary_key=True)  # 'id' from the properties
+    name = models.CharField(max_length=100)     # 'name' from the properties
+    location = models.PointField(geography=True)  # 'coordinates' as PointField
+
+    def __str__(self):
+        return self.name
+
+class State(models.Model):
+    id = models.IntegerField(primary_key=True)  # 'id' from the properties
+    name = models.CharField(max_length=100)     # 'name' from the properties
+    area = models.FloatField()                  # 'area' from the properties
+    geometry = models.MultiPolygonField(geography=True)  # 'geometry' as MultiPolygon
+
+    def __str__(self):
+        return self.name
+
+class River(models.Model):
+    id = models.CharField(primary_key=True)  # 'full_id' from the properties
+    name = models.CharField(max_length=100)    # 'name' from the properties
+    layer = models.CharField(max_length=255)   # 'layer' from the properties
+    path = models.CharField(max_length=500)    # 'path' from the properties
+    geometry = models.MultiLineStringField(geography=True)  # 'geometry' as MultiLineString
+
+    def __str__(self):
+        return self.name
